d0ca369e30a0288ab09d9679bad1e10c5e450659
Geoff Schmit <gcschmit@gmail.com>
2016-10-25 14:21:58 -0500

    finished chapter 6 notes
    also added a sample solution for the sumAndAverage algorithm

--- Chapter 6 Notes/Notes.java
+++ Chapter 6 Notes/Notes.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Notes
 {
     public static void whileExample()
@@ -106,6 +108,149 @@ public class Notes
         }
         
         System.out.println();
+    }
+    
+    public static void doExample()
+    {
+        /*
+         * do loop (do-while loop):
+         *      1. executes the body of the loop
+         *      2. evaluates the boolean codition
+         *          if true, executes the body of the loop again
+         *          if false, continues execution after the loop
+         */
+        
+        int count = 1;                      // initialization
+        
+        do
+        {
+            System.out.println( count );    // body
+            
+            count += 1;                     // update the condition variable
+        }
+        while( count <= 5 );                // condition
+    }
+    
+    public static void sum()
+    {
+        Scanner s = new Scanner( System.in );
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print( "enter a positive integer (-1 to quit): " );
+            /*
+             * Sentinel Value
+             *      value (e.g., -1) used to terminate a loop
+             *      it is often entered by a user
+             */
+            value = s.nextInt();
+            
+            sum += value;   // this is a bug! sum is 1 less than it shoudl be
+        }
+        while( value != -1 );
+        
+        System.out.println( "sum: " + sum );
+    }
+    
+    public static void sum2()
+    {
+        Scanner s = new Scanner( System.in );
+        
+        int sum = 0;
+        int value;
+        
+        do
+        {
+            System.out.print( "enter a positive integer (-1 to quit): " );
+            value = s.nextInt();
+            
+            if( value == -1 )
+            {
+                /*
+                 * break: exits the inner-most loop it is in
+                 *  (similiar behavior as used in switch statement)
+                 *  
+                 *  "we are done with this loop, right now"
+                 */
+                break;
+            }
+            
+            sum += value;
+        }
+        while( value != -1 );
+        
+        System.out.println( "sum: " + sum );
+    }
+    
+    public static void sum3()
+    {
+        Scanner s = new Scanner( System.in );
+        
+        int sum = 0;
+        int value = 0;
+        
+        do
+        {
+            System.out.print( "enter a positive integer (-1 to quit): " );
+            
+            if( s.hasNextInt() == false )
+            {
+                s.next();
+                
+                /*
+                 * continue:
+                 *      1. skips to the end of the inner-most loop it is in
+                 *      2. re-evaluates the loop condition at that point
+                 *      
+                 *  "we are done with this iteration; go to the next one"
+                 */
+                continue;
+            }
+            
+            value = s.nextInt();
+            
+            if( value == -1 )
+            {
+                break;
+            }
+            
+            sum += value;
+        }
+        while( value != -1 );
+        
+        System.out.println( "sum: " + sum );
+    }
+    
+        /*
+     * Loop Algorithm #1: Sum and Average
+     * reads a series of numbers
+     * calculates the sum and average of the numbers
+     */
+    public static void sumAndAverage()
+    {
+        Scanner s = new Scanner( System.in );
+        double sum = 0;
+        double average = 0;
+        int count = 0;
+        
+        System.out.println( "Enter a series of numbers (any letter to quit)" );
+        
+        while( s.hasNextDouble())
+        {
+            double value = s.nextDouble();
+            sum += value;
+            count++;
+        }
+        
+        if( count > 0 )
+        {
+            average = sum / count;
+        }
+        
+        System.out.println( "sum: " + sum + " average: " + average );
     }
     
 }
