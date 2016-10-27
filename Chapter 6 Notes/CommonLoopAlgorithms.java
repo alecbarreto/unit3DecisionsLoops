5a8e01db6652fe8c7e64c882e40d10aba7057994
Geoff Schmit <gcschmit@gmail.com>
2016-10-26 21:45:18 -0500

    added algorithms implemented by groups today

--- Chapter 6 Notes/CommonLoopAlgorithms.java
+++ Chapter 6 Notes/CommonLoopAlgorithms.java
@@ -14,24 +14,24 @@ public class CommonLoopAlgorithms
         double sum = 0;
         double average = 0;
         int count = 0;
-        
+
         System.out.println( "Enter a series of numbers (any letter to quit)" );
-        
+
         while( s.hasNextDouble())
         {
             double value = s.nextDouble();
             sum += value;
             count++;
         }
-        
+
         if( count > 0 )
         {
             average = sum / count;
         }
-        
+
         System.out.println( "sum: " + sum + " average: " + average );
     }
-    
+
     /*
      * Loop Algorithm #2: Counting Matches
      * reads a series of numbers (ends with a letter)
@@ -42,8 +42,26 @@ public class CommonLoopAlgorithms
      */
     public static void countMatches()
     {
+        Scanner s = new Scanner( System.in );
+        double number = 0;
+        int count = 0;
+
+        System.out.println( "Enter a series of numbers (any letter to quit)" );
+        while(s.hasNextDouble())
+        {
+
+            number = s.nextDouble();
+
+            if (number > 100)
+            {
+                count ++;
+            }
+            System.out.println( "Enter a series of numbers (any letter to quit)" );
+        }
+
+        System.out.println( "The number of numbers that are greater than 100:"+ count );
     }
-    
+
     /*
      * Loop Algorithm #3: Finding the First Match
      * reads a series of words separated by whitespace
@@ -55,7 +73,7 @@ public class CommonLoopAlgorithms
     public static void findFirstMatch()
     {
     }
-    
+
     /*
      * Loop Algorithm #4: Prompting until a Match Is Found
      * prompts the user to enter a positive integer less than 100
@@ -67,8 +85,23 @@ public class CommonLoopAlgorithms
      */
     public static void promptUntilMatch()
     {
+        Scanner s = new Scanner( System.in );
+        double userNum = 101.0;
+
+        while (userNum >= 100) 
+        {
+            System.out.println( "Enter a number(intergers only): " );
+            userNum = s.nextDouble();
+
+            if ((userNum <= -1) || ((userNum%1)>0))
+            {
+                userNum = 101;
+            }
+        }
+
+        System.out.println( (int)userNum );
     }
-    
+
     /*
      * Loop Algorithm #5.1: findMax
      * reads a series of numbers (ends with a letter)
@@ -76,10 +109,21 @@ public class CommonLoopAlgorithms
      * 
      * PURPLE GROUP
      */
-    public static void findMax()
+    public static void findMax() 
     {
+        Scanner sc = new Scanner(System.in);
+        int greatest = 0;
+        do {
+            System.out.println("Please enter an integer: ");
+            int find = sc.nextInt();
+            if (find > greatest) {
+                greatest = find;
+            }
+        }
+        while (sc.hasNextInt() == true);
+        System.out.println("The greatest integer is " + greatest);
     }
-    
+
     /*
      * Loop Algorithm #5.2: findMin
      * reads a series of numbers (ends with a letter)
@@ -89,8 +133,32 @@ public class CommonLoopAlgorithms
      */
     public static void findMin()
     {
+        Scanner s = new Scanner( System.in );
+        System.out.print("Enter a positive integer: ");
+        int val = s.nextInt();
+        int min = val;
+
+        do 
+        {
+            System.out.print("Enter a positive integer: ");
+
+            if(!(s.hasNextInt()))
+            {
+                break;
+            }
+            val = s.nextInt();
+            if (val<min)
+            {
+                min = val;
+            }
+
+        }
+
+        while(val != 0);
+
+        System.out.print("Min: " + min);
     }
-    
+
     /*
      * Loop Algorithm #6: Compare Adjacent Values
      * reads a series of numbers (ends with a letter)
@@ -100,7 +168,21 @@ public class CommonLoopAlgorithms
      */
     public static void compareAdjacent()
     {
+        int old;
+        int new1;
+
+        Scanner s = new Scanner( System.in );
+        System.out.print("Enter a number: ");
+        new1 = s.nextInt();
+        do {
+            old = new1;
+            System.out.print("Enter a number: ");
+            if(s.hasNextInt()==false)
+            {
+
+            }
+        }while (new1 !=1);
+
     }
-    
-    
+
 }
